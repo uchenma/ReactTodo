824c2928b32a621d722a7eb145c5f0ccfacfacfc
uchenma <uchenm@seas.upenn.edu>
2019-07-18 15:33:09 -0400

    Static version

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,45 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-ReactDOM.render(<p>React lives!</p>,
+let dummyData =["Cancel amazon subscription", "Buy new headphones", "Meal prep for next week", "Renew gym membership"]
+
+class Todo extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <li>
+                <input type="submit" value="X"></input>
+                {this.props.task}
+            </li>
+            
+        )
+    }
+}
+class TodoList extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <ul>
+                {dummyData.map((todo)=> <Todo task={todo}/>)}
+            </ul>
+        )
+        
+    }
+        
+    
+}
+class InputLine extends React.Component{
+    
+}
+class TodoApp extends React.Component{
+    
+}
+
+
+
+ReactDOM.render(<TodoList/>,
     document.getElementById('root'));
